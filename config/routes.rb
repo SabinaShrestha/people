1446a338cc8f2fe578b9f16ed4907150ee288d34
Sabina Shrestha <sabina.pari@yahoo.com>
2018-05-29 16:59:20 -0600

    add children controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'children/index'
+  get 'children/show'
+  get 'children/new'
+  get 'children/edit'
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
-  resources 'people'
+  resources :people do
+    resources :children
+  end
 end
